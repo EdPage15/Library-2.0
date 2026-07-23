082b9221a5a13127085a6fbcf26b485d633512ac
EdPage15 <edpage985@gmail.com>
2026-07-22 23:27:02 -0500

    Ready for deployment

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,63 +1,63 @@
-import React, { useState, useEffect, useRef } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Rating from "./Rating"
-import Price from "./Price"
+import Rating from "./Rating";
+import Price from "./Price";
 
 const Book = ({ book }) => {
-    const [img, setImg] = useState();
+  const [isLoaded, setIsLoaded] = useState(false);
 
-    const mountedRef = useRef(true);
+  useEffect(() => {
+    let isMounted = true;
+    const image = new Image();
+    image.src = book.url;
 
-    useEffect(() => {
-        const image = new Image();
-        image.src = book.url;
-        image.onload = () => {
-            setTimeout(() => {
-                if (mountedRef.current) {
-                    setImg(image);
-                }
-            }, 300);
-        };
-        return () => {
-            mountedRef.current = false;
+    image.onload = () => {
+      setTimeout(() => {
+        if (isMounted) {
+          setIsLoaded(true);
         }
-    })
+      }, 300);
+    };
 
-    return (
-        <div className="book">
-            {img ? (
-                <>
-                    <Link to={`/books/${book.id}`}>
-                        <figure className="book__img--wrapper">
-                            <img 
-                                src={img.src} 
-                                alt="" 
-                                className="book__img" 
-                            />
-                        </figure>
-                    </Link>
-                    <div className="book__title">
-                        <Link to={`/books/${book.id}`} className="book__title--link">
-                            {book.title}
-                        </Link>
-                    </div>
-                    <Rating rating={book.rating} />
-                    <Price 
-                        salePrice={book.salePrice} 
-                        originalPrice={book.originalPrice} 
-                    />
-                </>
-            ) : (
-                <>
-                    <div className="book__img--skeleton"></div>
-                    <div className="skeleton book__title--skeleton"></div>
-                    <div className="skeleton book__rating--skeleton"></div>
-                    <div className="skeleton book__price--skeleton"></div>
-                </>
-            )}
-        </div>
-    )
-}
+    return () => {
+      isMounted = false; // Prevents state updates if the component unmounts
+    };
+  }, [book.url]); // Dependency array ensures this only runs when the URL changes
+
+  return (
+    <div className="book">
+      {isLoaded ? (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img 
+                src={book.url} 
+                alt={book.title} 
+                className="book__img" 
+              />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating} />
+          <Price 
+            salePrice={book.salePrice} 
+            originalPrice={book.originalPrice} 
+          />
+        </>
+      ) : (
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Book;
